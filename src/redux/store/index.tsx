32e775f0c16b56d5c84384b623bc723d31ac961f
arman-ag <alighanbari.1994@gmail.com>
2023-05-19 14:39:13 +0330

    style: :fire: Remove redux dev tools conection

--- src/redux/store/index.tsx
+++ src/redux/store/index.tsx
@@ -1,9 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import allReducer from '../reducer';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(
-  allReducer,
-  composeEnhancers(applyMiddleware(thunkMiddleware))
-);
+export const store = createStore(allReducer, applyMiddleware(thunkMiddleware));
