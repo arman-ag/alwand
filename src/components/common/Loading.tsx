0e1cedd7eb4ce3ce06817e21baf294b8730bd66d
arman-ag <alighanbari.1994@gmail.com>
2023-05-19 14:56:03 +0330

    style: :lipstick: Change loading style

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,10 +1,10 @@
 const Loading = () => {
   return (
-    <div className='text-center h-full'>
-      <div className='h-full' role='status '>
+    <div className='text-center '>
+      <div role='status '>
         <svg
           aria-hidden='true'
-          className='relative top-1/2 inline w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
+          className='absolute  top-1/2 inline w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600'
           viewBox='0 0 100 101'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
